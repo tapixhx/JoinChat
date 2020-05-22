26c997bd626758dcee8ea794fc08dfc090910550
tapixhx <tapish818@gmail.com>
2020-05-23 00:06:28 +0530

    Home page and routing done with some optimisations

--- chat-client/src/app/app-routing.module.ts
+++ chat-client/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoomComponent } from './room/room.component';
 import { ChatComponent } from './chat/chat.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 const appRoutes: Routes=[
-    {path:'', redirectTo:'homepage',pathMatch:'full'},
+    {path:'', component:HomeComponent},
     {path:'room/:id',component:RoomComponent},
     {path:'room',component:RoomComponent},
     {path:'chat',component:ChatComponent},
